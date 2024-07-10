58bc726aaf0f31f89cdb3a610861a23cd8236819
root <root@DESKTOP-BUGKGO7>
2024-07-11 00:08:42 +0200

    media page populated with all articles

--- src/app/media/page.jsx
+++ src/app/media/page.jsx
@@ -1,5 +1,6 @@
 import Gallery from "./Gallery";
 import MediaItems from "./MediaItems";
+import { createClient } from '@prismicio'
 
 const articleList = [
   {
@@ -31,9 +32,35 @@ export const metadata = {
   title:'Media'
 }
 
+export default async function AllPosts () {
+  const client = createClient();
+  const articles_arr = await client.getAllByType("article");
+
+  return ( 
+	   <section>
+		<h1>All Posts</h1>
+		<article className='flex overflow-x-scroll'>
+	  	{articles_arr.map((el) => (
+		    <Post
+			    image={el.data.story_image}
+			    title={el.data.title}
+            		    content={el.data.story}
+            		    author={el.data.author}
+            	 	    date={el.data.publishing_time}
+            	 	    link={el.url}
+		     />
+		))}
+		</article>
+	    </section>
+  	);
+}
+
 const Media = () => {
 return (
   <main>
+    /*The component above generates articles like on the home page but queries all of them.
+      You can just call it here.
+    */
     <MediaItems props = {articleList} />
     <MediaItems props = {newsletterList} />
     <Gallery />
