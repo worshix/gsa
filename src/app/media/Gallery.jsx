ef3b0a16c6c2e9527149b6a63f1245406cafd047
worshix <worshiplivingstone@gmail.com>
2024-07-10 22:29:08 -0700

    gallery skeleton

---
+++ src/app/media/Gallery.jsx
@@ -0,0 +1,24 @@
+// for the gallery
+
+const imageList = [ // list of the images, without links obvously
+    {
+        id:'',
+        alt:'',
+        imagePath:''
+    },
+    {
+        id:'',
+        alt:'',
+        imagePath:''
+    }
+]
+
+const Gallery = () => {
+  return (
+    <section>
+      {/* Use the image listto make gallery */}
+    </section>
+  )
+}
+
+export default Gallery
---
+++ src/app/media/Media.jsx
@@ -0,0 +1,11 @@
+// for Newsletters an articles
+
+const Media = (props) => {
+  return (
+    <section className="">
+        {/* code to make use of the props */}
+    </section>
+  )
+}
+
+export default Media
