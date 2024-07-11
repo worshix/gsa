03d648b45f5ba4336fcd1617467f5e52667bca2b
worshix <worshiplivingstone@gmail.com>
2024-07-10 23:33:30 -0700

    mediaItems

---
+++ src/app/media/MediaItems.jsx
@@ -0,0 +1,11 @@
+// for Newsletters an articles
+
+const MediaItems = (props) => {
+  return (
+    <section className="">
+        {/* code to make use of the props */}
+    </section>
+  )
+}
+
+export default MediaItems
