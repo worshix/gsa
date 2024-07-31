b3de4b98efcbf0975d967f7dc9a874979c0daa28
worshix <worshiplivingstone@gmail.com>
2024-07-31 00:29:57 -0700

    added other routes for media page

---
+++ src/app/media/news/page.jsx
@@ -0,0 +1,32 @@
+import Post from '@/components/latestPosts/Post';
+import { createClient } from '@/prismicio';
+
+export default async function LatestPosts () {
+  const client = createClient();
+  const articles_arr = await client.getAllByType("article", {
+	  limit: 10,
+  	  orderings: [
+    	  {
+      	    field: 'my.article.publishing_time',
+      	    direction: 'desc',
+    	  },
+  	],
+  });
+  return ( 
+	   <section>
+	<h1 className="text-4xl font-bold my-4 text-center text-main-400 w-full flex-shrink-0">News</h1>
+    <article className='flex overflow-x-scroll p-3 flex-col gap-2 sm:flex-row flex-wrap sm:justify-center'>
+			{articles_arr.map((el) => (
+				<Post
+					image={el.data.story_image}
+					title={el.data.title}
+					content={el.data.story}
+					author={el.data.author}
+					date={el.data.publishing_time}
+					link={el.url}
+				/>
+			))}
+			</article>
+	    </section>
+  	);
+}
