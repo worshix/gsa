f3b7ae2ca2058b53c66d3750449e05d464fdb54a
worshix <worshiplivingstone@gmail.com>
2024-08-09 21:03:26 -0700

    landing images okay

--- src/components/LandingGallery.jsx
+++ src/components/LandingGallery.jsx
@@ -4,19 +4,24 @@ import { PrismicNextImage } from '@prismicio/next';
 
 const LandingGallery = async () => {
     const client = createClient();
-    const images = await client.getAllByType("image_gallery", {limit: 5});  
-  return (
-    <article className='images-container pt-6 p-2 flex' id="images-container">
-        {
-        images.map((image,index) => {
-            return(
-                <div key={"div-"+index} className='slide duration-200 max-w-[500px]'>
-                    <PrismicNextImage field={image.data.image} key={`landing-image-${index+1}`} className={`rounded-md shadow-md shadow-black gallery-image-${index+1}`} />               
-                </div>      
-            )}
-    )}
-    </article>
-  )
+    const images = await client.getAllByType("image_gallery", { limit: 5 });
+  
+    return (
+        <article className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-4" id="images-container">
+            {images.map((image, index) => (
+                <div 
+                    key={"div-" + index} 
+                    className={`slide duration-200 h-64 lg:h-80 relative overflow-hidden rounded-md shadow-md shadow-black group`}
+                >
+                    <PrismicNextImage 
+                        field={image.data.image} 
+                        key={`landing-image-${index+1}`} 
+                        className="w-full h-full object-cover transform group-hover:scale-105 transition-transform duration-300" 
+                    />
+                </div>
+            ))}
+        </article>
+    );
 }
 
-export default LandingGallery
+export default LandingGallery;
