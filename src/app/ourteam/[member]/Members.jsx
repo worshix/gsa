27aed59b541f6aa8dd3fd1e04e4ba84443836723
worshix <worshiplivingstone@gmail.com>
2024-08-09 23:41:12 -0700

    members typos gone

--- src/app/ourteam/[member]/Members.jsx
+++ src/app/ourteam/[member]/Members.jsx
@@ -15,24 +15,24 @@ const Members = {
             },
             bio:[
                 'Ngoni Veddie Muzondo, a dedicated and driven futuristic pharmacist, demonstrates a strong'+
-                'commitment to scientific research and global health advocacy. Passionate about'+
-                'antimicrobial resistance (AMR), sexual reproductive health, supply chain management, and'+
-                'global health, Ngoni&#39;s interest in AMR stems from academic exposure and global concerns.'+
-                'He is a graduate of the Antimicrobial Resistance Leadership Program under the guard of'+
-                'ReAct Africa. Recognizing the urgency of combating AMR, Ngoni co-founded Generational'+
-                'Stewards for Antimicrobials as the head of programs and team lead to raise awareness and'+
-                'promote responsible antibiotic use among primary and secondary students. For the past two'+
-                'years, he has been the youth coordinator for the World Antimicrobial Awareness Week'+
-                '(WAAW) Zimbabwe National Organizing Committee. He has served as a Contact Person of'+
-                'the International Pharmaceutical Students Federation (IPSF) to the Zimbabwe'+
-                'Pharmaceutical Students Association (ZPSA) for the past two years.  Advocating for sexual'+
-                'reproductive health and rights, with a focus on education, healthcare access, and gender'+
-                'equality, Ngoni prioritizes empowering vulnerable populations and respecting their rights and'+
-                'is a Sexual Reproductive Health and Rights (SRHR) peer educator under the training of'+
-                'SRHR Africa Trust (SAT). He is an alumnus of the African Middle Eastern Leadership Project'+
-                '(AMELI), the United Nations Millennium Campus Network (UN MCN), and the Aspire'+
-                'Leadership Program, all of which helped him obtain excellent leadership experience and'+
-                'International diplomacy.',
+                ' commitment to scientific research and global health advocacy. Passionate about'+
+                ' antimicrobial resistance (AMR), sexual reproductive health, supply chain management, and'+
+                ' global health, Ngoni\'s interest in AMR stems from academic exposure and global concerns.'+
+                ' He is a graduate of the Antimicrobial Resistance Leadership Program under the guard of'+
+                ' ReAct Africa. Recognizing the urgency of combating AMR, Ngoni co-founded Generational'+
+                ' Stewards for Antimicrobials as the head of programs and team lead to raise awareness and'+
+                ' promote responsible antibiotic use among primary and secondary students. For the past two'+
+                ' years, he has been the youth coordinator for the World Antimicrobial Awareness Week'+
+                ' (WAAW) Zimbabwe National Organizing Committee. He has served as a Contact Person of'+
+                ' the International Pharmaceutical Students Federation (IPSF) to the Zimbabwe'+
+                ' Pharmaceutical Students Association (ZPSA) for the past two years.  Advocating for sexual'+
+                ' reproductive health and rights, with a focus on education, healthcare access, and gender'+
+                ' equality, Ngoni prioritizes empowering vulnerable populations and respecting their rights and'+
+                ' is a Sexual Reproductive Health and Rights (SRHR) peer educator under the training of'+
+                ' SRHR Africa Trust (SAT). He is an alumnus of the African Middle Eastern Leadership Project'+
+                ' (AMELI), the United Nations Millennium Campus Network (UN MCN), and the Aspire'+
+                ' Leadership Program, all of which helped him obtain excellent leadership experience and'+
+                ' International diplomacy.',
             ]
         },
         munyaradzichisora:{
@@ -50,16 +50,16 @@ const Members = {
                 instagram:null,
             },
             bio:[
-                'Munyaradzi Chisora is a passionate and driven individual with a strong commitment'+
-                'to social impact. He is a soon-to-be graduate in pharmaceutical sciences from the'+
-                'University of Zimbabwe, where he has honed his scientific knowledge and analytical'+
-                'skills. As co-founder and head of events planning for GSA, he demonstrates his leadership'+
-                'abilities and dedication to community engagement. He is also an alumnus of the Aspire'+
-                'Leaders Program and UN Millennium Fellowship, showcasing his commitment to global'+
-                'development. Driven by a desire to empower individuals, Munyaradzi founded Acorn Africa'+
-                'Connect, a consultancy specializing in soft skills development. He is particularly'+
-                'interested in Sexual and Reproductive Health and Rights (SRHR) and HIV/AIDS, reflecting'+
-                'his commitment to improving health outcomes for all.'
+                ' Munyaradzi Chisora is a passionate and driven individual with a strong commitment'+
+                ' to social impact. He is a soon-to-be graduate in pharmaceutical sciences from the'+
+                ' University of Zimbabwe, where he has honed his scientific knowledge and analytical'+
+                ' skills. As co-founder and head of events planning for GSA, he demonstrates his leadership'+
+                ' abilities and dedication to community engagement. He is also an alumnus of the Aspire'+
+                ' Leaders Program and UN Millennium Fellowship, showcasing his commitment to global'+
+                ' development. Driven by a desire to empower individuals, Munyaradzi founded Acorn Africa'+
+                ' Connect, a consultancy specializing in soft skills development. He is particularly'+
+                ' interested in Sexual and Reproductive Health and Rights (SRHR) and HIV/AIDS, reflecting'+
+                ' his commitment to improving health outcomes for all.'
             ]
         },
         kupakwashenkomo:{
@@ -77,11 +77,11 @@ const Members = {
                 instagram:null,
             },
             bio:[
-                'I am a Biotechnologist and in GSA I’m the Monitoring and Evaluation Officer. As the Monitoring'+
-                'and Evaluation (M&E) Officer im focused on ensuring the efficacy and efficiency of antimicrobial'+
-                'programs and interventions. As antimicrobial resistance continues to pose a significant threat to'+
-                'global health, my responsibilities of an M&E Officer become increasingly critical in the fight'+
-                'against this challenge.'
+                ' I am a Biotechnologist and in GSA I’m the Monitoring and Evaluation Officer. As the Monitoring'+
+                ' and Evaluation (M&E) Officer im focused on ensuring the efficacy and efficiency of antimicrobial'+
+                ' programs and interventions. As antimicrobial resistance continues to pose a significant threat to'+
+                ' global health, my responsibilities of an M&E Officer become increasingly critical in the fight'+
+                ' against this challenge.'
             ]
         },
         brandonjaka:{
@@ -99,14 +99,14 @@ const Members = {
                 instagram:'https://www.instagram.com/jb_eulacx?igsh=d210b3c4MGlxYmth',
             },
             bio: [
-                'Brandon is a young healthcare professional who has almost 2 years working in the pharmacy'+
-                'field as a community pharmacist. He is a member of the WHO Taskforce of AMR Survivors which'+
-                'seeks to amplify patients\' voices and share stories and experiences that can help shape the AMR'+
-                'narrative. He is also a member of Pharmacists Against Drug Abuse, a group of pharmacists that'+
-                'advocate against drug and substance abuse, including abuse of antimicrobials. At GSA, Brandon'+
-                'is a Media advisor working on visuals and designs which encompass fliers, posters, branding e.t.c'+
-                'Brandon is passionate about Global health and advocates for SDG#3 which calls for good health and'+
-                'well-being.',
+                ' Brandon is a young healthcare professional who has almost 2 years working in the pharmacy'+
+                ' field as a community pharmacist. He is a member of the WHO Taskforce of AMR Survivors which'+
+                ' seeks to amplify patients\' voices and share stories and experiences that can help shape the AMR'+
+                ' narrative. He is also a member of Pharmacists Against Drug Abuse, a group of pharmacists that'+
+                ' advocate against drug and substance abuse, including abuse of antimicrobials. At GSA, Brandon'+
+                ' is a Media advisor working on visuals and designs which encompass fliers, posters, branding e.t.c'+
+                ' Brandon is passionate about Global health and advocates for SDGs which calls for good health and'+
+                ' well-being.',
             ]
         },
         tafadzwachirisa:{
@@ -142,20 +142,20 @@ const Members = {
                 instagram:null,
             },
             bio: [
-                'Munyaradzi Innocent is a dedicated and passionate individual pursuing a Bachelor of Technology Honours Degree'+
-                'in Biotechnology at Harare Institute of Technology, where he is currently in his final year. His love for science and'+
-                'mathematics has been evident from a young age, and he is particularly interested in the field of bioinformatics.'+
-                'Munyaradzi&#39;s commitment to his work and his ability to collaborate effectively have been instrumental in his'+
-                'success. He has demonstrated strong leadership skills and has obtained numerous certifications in leadership.',
+                ' Munyaradzi Innocent is a dedicated and passionate individual pursuing a Bachelor of Technology Honours Degree'+
+                ' in Biotechnology at Harare Institute of Technology, where he is currently in his final year. His love for science and'+
+                ' mathematics has been evident from a young age, and he is particularly interested in the field of bioinformatics.'+
+                ' Munyaradzi\'s commitment to his work and his ability to collaborate effectively have been instrumental in his'+
+                ' success. He has demonstrated strong leadership skills and has obtained numerous certifications in leadership.',
                 
-                'During his internship period, Munyaradzi worked as a QA &amp; QC laboratory technician at Zimgold Pure Oil'+
-                'Industries for 12 months, gaining valuable practical experience. He is currently serving as the clubs coordinator '+
-                'the Generational Stewards for Antimicrobials (GSA) clubs. Prior to this role, he was a member of Ndinewe Mental'+
-                'Health Club. His involvement in these organizations showcases his dedication to making a positive impact on his'+
-                'community and his commitment to promoting mental health awareness and antimicrobial stewardship.',
+                ' During his internship period, Munyaradzi worked as a QA & QC laboratory technician at Zimgold Pure Oil'+
+                ' Industries for 12 months, gaining valuable practical experience. He is currently serving as the clubs coordinator '+
+                ' the Generational Stewards for Antimicrobials (GSA) clubs. Prior to this role, he was a member of Ndinewe Mental'+
+                ' Health Club. His involvement in these organizations showcases his dedication to making a positive impact on his'+
+                ' community and his commitment to promoting mental health awareness and antimicrobial stewardship.',
 
-                'As an aspiring biotechnologist, he is passionate about conducting research in various scientific aspects. His humble'+
-                'nature and strong work ethic make him a valuable asset to any organization he is a part of.'
+                ' As an aspiring biotechnologist, he is passionate about conducting research in various scientific aspects. His humble'+
+                ' nature and strong work ethic make him a valuable asset to any organization he is a part of.'
             ]
         },
         innocentnjazi:{
@@ -194,14 +194,14 @@ const Members = {
                 instagram:null,
             },
             bio:[
-                'Olive Thomas is a medical laboratory scientist who currently serves as the Communications'+
-                'Director at Generational Stewards for Antimicrobials. In this role, she orchestrates the'+ 
-                'flow of information between the organization and the public, including championing a smooth'+
-                'moderation of GSA\'s fortnight webinars of global conversations.'+ 
-                'Olive is a global public health enthusiast who aspires to make meaningful contributions in global health'+
-                'initiatives aiming to promote wellness in communities through research, education and advocacy.'+ 
-                'Olive\'s commitment to personal and professional development is evident through her participation in the'+
-                'YALI RLC SA programme, further equipping herself to drive positive change and contribute to GSA\'s success.'
+                ' Olive Thomas is a medical laboratory scientist who currently serves as the Communications'+
+                ' Director at Generational Stewards for Antimicrobials. In this role, she orchestrates the'+ 
+                ' flow of information between the organization and the public, including championing a smooth'+
+                ' moderation of GSA\'s fortnight webinars of global conversations.'+ 
+                ' Olive is a global public health enthusiast who aspires to make meaningful contributions in global health'+
+                ' initiatives aiming to promote wellness in communities through research, education and advocacy.'+ 
+                ' Olive\'s commitment to personal and professional development is evident through her participation in the'+
+                ' YALI RLC SA programme, further equipping herself to drive positive change and contribute to GSA\'s success.'
             ]
         },
         oscarmano: {
@@ -281,8 +281,8 @@ const Members = {
         },
         michelleheiden: {
             id:'michelleheiden',
-            name:'Michelle Van Der',
-            surname:'Heiden',
+            name:'Michelle',
+            surname:'Heiden Van Der',
             title:'Secretary General',
             imageExtention:'jpg',
             socials:{
