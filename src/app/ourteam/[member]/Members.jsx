d97762a716d136f8f02ce73fd1e00d868009bc22
worshix <worshiplivingstone@gmail.com>
2024-08-14 11:34:48 -0700

    changed Munyaradzi Chisora's Image Extension

--- src/app/ourteam/[member]/Members.jsx
+++ src/app/ourteam/[member]/Members.jsx
@@ -40,7 +40,7 @@ const Members = {
             name:'Munyaradzi',
             surname:'Chisora',
             title:'Co-founder/ Events Planning Officer',
-            imageExtention:'jpg',
+            imageExtention:'JPG',
             socials:{
                 twitter:null,
                 email:'munya.sachis@gmail.com',
